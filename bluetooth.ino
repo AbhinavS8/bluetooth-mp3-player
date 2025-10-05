637cda9f3f5464202ee20d7b01406ea7a3335c93
Abhinav Subramanian <abhinav.subr@gmail.com>
2025-10-05 17:32:14 +0530

    Implement volume control with potentiometer

--- bluetooth.ino
+++ bluetooth.ino
@@ -14,6 +14,7 @@
 #define playPauseBtn 25
 #define nextBtn 26
 #define prevBtn 27
+#define potPin 36
 
 const char *startFilePath="/";
 const char* ext="mp3";
@@ -27,6 +28,8 @@ const unsigned long debounceDelay = 200;
 unsigned long lastPlayPause = 0;
 unsigned long lastNext = 0;
 unsigned long lastPrev = 0;
+float currentVolume = 0.5;
+float lastVolume = 0.5;
 
 void setup() {
   Serial.begin(115200);
@@ -44,14 +47,15 @@ void setup() {
   // setup player
   // Setting up SPI if necessary with the right SD pins by calling 
   // SPI.begin(PIN_AUDIO_KIT_SD_CARD_CLK, PIN_AUDIO_KIT_SD_CARD_MISO, PIN_AUDIO_KIT_SD_CARD_MOSI, PIN_AUDIO_KIT_SD_CARD_CS);
-  player.setVolume(0.5);
+  pinMode(potPin, INPUT);
+  player.setVolume(currentVolume);
   player.begin();
 
   // setup output - We send the test signal via A2DP - so we conect to a Bluetooth Speaker
   auto cfg = out.defaultConfig(TX_MODE);
   cfg.silence_on_nodata = true; // prevent disconnect when there is no audio data
   cfg.name = "Jabra Elite Active 65t";  // set the device here. Otherwise the first available device is used for output
-  //cfg.auto_reconnect = true;  // if this is use we just quickly connect to the last device ignoring cfg.name
+  // cfg.auto_reconnect = true;  // if this is use we just quickly connect to the last device ignoring cfg.name
   out.begin(cfg);
 
 
@@ -59,7 +63,7 @@ void setup() {
 
 void loop() {
   // Play/Pause button
-if (digitalRead(playPauseBtn) == LOW && (millis() - lastPlayPause > debounceDelay)) {
+  if (digitalRead(playPauseBtn) == LOW && (millis() - lastPlayPause > debounceDelay)) {
     lastPlayPause = millis();
     if (player.isActive()) {
       player.stop();
@@ -69,15 +73,23 @@ if (digitalRead(playPauseBtn) == LOW && (millis() - lastPlayPause > debounceDela
   }
 
   // next song button
-if (digitalRead(nextBtn) == LOW && (millis() - lastNext > debounceDelay)) {
+  if (digitalRead(nextBtn) == LOW && (millis() - lastNext > debounceDelay)) {
     lastNext = millis();
     player.next();
   }
 
     // prev song button
-if (digitalRead(prevBtn) == LOW && (millis() - lastPrev > debounceDelay)) {
+  if (digitalRead(prevBtn) == LOW && (millis() - lastPrev > debounceDelay)) {
     lastPrev   = millis();
     player.previous();
+  }
+
+  int potValue = analogRead(potPin);  // 0 - 4095
+  float newVolume = potValue / 4095.0;
+
+  if (abs(newVolume - lastVolume) > 0.02) {
+    player.setVolume(newVolume);
+    lastVolume = newVolume;
   }
 
   player.copy();
