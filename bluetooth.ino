92a9c2bd94da8eef8d163681b6609d30546bda9a
Abhinav Subramanian <abhinav.subr@gmail.com>
2026-05-28 19:27:05 +0530

    Implement volume control in bluetooth.ino

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,7 +1,10 @@
 /**
  * @file player-sdfat-a2dp.ino
  * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-player/player-sdfat-a2dp/README.md
-*/
+ * 
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
 
 //remove this, to find issues regarding mp3 decoding
 // #define HELIX_LOGGING_ACTIVE false
@@ -29,6 +32,10 @@ unsigned long lastNext = 0;
 unsigned long lastPrev = 0;
 bool paused = false;
 
+// volume
+const int potPin = 35;
+float lastVolume = 0.8;
+
 void setup() {
   Serial.begin(115200);
   AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Warning);
@@ -36,12 +43,14 @@ void setup() {
   pinMode(playPauseBtn, INPUT_PULLUP);
   pinMode(nextBtn, INPUT_PULLUP);
   pinMode(prevBtn, INPUT_PULLUP);
+  pinMode(potPin, INPUT);
   // setup player
   // Setting up SPI if necessary with the right SD pins by calling 
   // SPI.begin(PIN_AUDIO_KIT_SD_CARD_CLK, PIN_AUDIO_KIT_SD_CARD_MISO, PIN_AUDIO_KIT_SD_CARD_MOSI, PIN_AUDIO_KIT_SD_CARD_CS);
   player.setVolume(0.8);
   player.begin();
 
+  // setup output - We send the test signal via A2DP - so we conect to a Bluetooth Speaker
   auto cfg = out.defaultConfig(TX_MODE);
   cfg.silence_on_nodata = true; // prevent disconnect when there is no audio data
   cfg.name = "";  // set the device here. Otherwise the first available device is used for output
@@ -54,6 +63,23 @@ void setup() {
 
 void loop() {
 
+
+  // --- Volume Control ---
+  int potValue = analogRead(potPin);
+
+  // Convert 0-4095 -> 0.0-1.0
+  float volume = potValue / 4095.0f;
+
+  // Prevent constant tiny updates
+  if (abs(volume - lastVolume) > 0.02f) {
+
+      player.setVolume(volume);
+
+      Serial.print("Volume: ");
+      Serial.println(volume);
+
+      lastVolume = volume;
+  }
   // --- Play / Pause ---
   if (digitalRead(playPauseBtn) == LOW &&
       millis() - lastPlayPause > debounceDelay) {
